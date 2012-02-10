2e42c95f8192d5d6a6869d71ce1ae21f04cc3f0b
Jascha Ehrenreich <jascha@jaeh.at>
2012-02-10 18:23:04 +0100

    index.html, index.css, jquery.amp.js: added content fadein when turning the amp on, fade out when turning off. should lead to less confusion for the users.
    metalogo.svg, meta-logo.png:
    image for the logo, not transparent and colored using css

--- www/_inc/js/jquery.amp.js
+++ www/_inc/js/jquery.amp.js
@@ -25,7 +25,7 @@ var xml_get_status_url = "http://10.20.30.51/cgi-bin/getstatus";
 var xml_get_input_url = "http://10.20.30.51/cgi-bin/getinput";
 
 // default xml string to test locally
-var xmlstats = '<YAMAHA_AV rsp="GET" RC="0"><Main_Zone><Basic_Status><Power_Control><Power>On</Power><Sleep>Off</Sleep></Power_Control><Volume><Lvl><Val>-185</Val><Exp>1</Exp><Unit>dB</Unit></Lvl><Mute>Off</Mute></Volume><Input><Input_Sel>AUDIO3</Input_Sel><Input_Sel_Item_Info><Param>AUDIO1</Param><RW>RW</RW><Title>AUDIO1</Title><Icon><On>/YamahaRemoteControl/Icons/icon002.png</On><Off></Off></Icon><Src_Name></Src_Name><Src_Number>1</Src_Number></Input_Sel_Item_Info></Input><Surround><Program_Sel><Current><Straight>Off</Straight><Enhancer>On</Enhancer><Sound_Program>7ch Stereo</Sound_Program></Current></Program_Sel><_3D_Cinema_DSP>Auto</_3D_Cinema_DSP><Dialogue_Lift>0</Dialogue_Lift></Surround><Pure_Direct><Mode>Off</Mode></Pure_Direct><Sound_Video><Tone><Bass><Val>50</Val><Exp>1</Exp><Unit>dB</Unit></Bass><Treble><Val>-50</Val><Exp>1</Exp><Unit>dB</Unit></Treble></Tone><Adaptive_DRC>Off</Adaptive_DRC></Sound_Video></Basic_Status></Main_Zone></YAMAHA_AV>';
+var xmlstats = '<YAMAHA_AV rsp="GET" RC="0"><Main_Zone><Basic_Status><Power_Control><Power>Off</Power><Sleep>Off</Sleep></Power_Control><Volume><Lvl><Val>-185</Val><Exp>1</Exp><Unit>dB</Unit></Lvl><Mute>Off</Mute></Volume><Input><Input_Sel>AUDIO3</Input_Sel><Input_Sel_Item_Info><Param>AUDIO1</Param><RW>RW</RW><Title>AUDIO1</Title><Icon><On>/YamahaRemoteControl/Icons/icon002.png</On><Off></Off></Icon><Src_Name></Src_Name><Src_Number>1</Src_Number></Input_Sel_Item_Info></Input><Surround><Program_Sel><Current><Straight>Off</Straight><Enhancer>On</Enhancer><Sound_Program>7ch Stereo</Sound_Program></Current></Program_Sel><_3D_Cinema_DSP>Auto</_3D_Cinema_DSP><Dialogue_Lift>0</Dialogue_Lift></Surround><Pure_Direct><Mode>Off</Mode></Pure_Direct><Sound_Video><Tone><Bass><Val>50</Val><Exp>1</Exp><Unit>dB</Unit></Bass><Treble><Val>-50</Val><Exp>1</Exp><Unit>dB</Unit></Treble></Tone><Adaptive_DRC>Off</Adaptive_DRC></Sound_Video></Basic_Status></Main_Zone></YAMAHA_AV>';
 
 /******************************************* DOCUMENT.READY **********************************************/
 $(document).ready( function ($) {
@@ -38,6 +38,8 @@ $(document).ready( function ($) {
 	t.volume_to_set = 0;
 	t.max_bass = 50;
 	t.max_treble = 50;
+	
+	t.on = false;
 	
 	t.search_and_play_visible = false;
 	t.upload_stream_visible = false;
@@ -74,15 +76,16 @@ function renderXML (xml, t ) {
 
 	//show the button according to the settings
 	if ( power.text() == "On" ) {
-		$('#off').show();
-		$('#on').hide();
-	} else /*if ( power.text() == "Standby" )*/ {
-		$('#on').show();
-		$('#off').hide();
+		$('#meta-logo').removeClass('off').addClass('on');
+		
+		$('#content').animate({'height': 'toogle', 'opacity': 1 }, 500).removeClass('hidden');
+		// hide the "loading" message
+		$('#on-off-info').empty();
+		t.on = true;
+	} else {
+		$('#on-off-info').html('Lounge Control is deactivated. Press the logo to activate.');
+		t.on = false;
 	}
-	// hide the "loading" message
-	$('#on-off-info').empty();
-	
 	
 	/************* populating default html values ***********/
 	var input = $(xml).find( 'Input_Sel' ).text();
@@ -133,20 +136,25 @@ function renderXML (xml, t ) {
 	
 	
 	//turns the amplifier off
-	$('#off').live('click', function() {
-		$.get ( urlprefix+'cgi-bin/off' );
-		$(this).hide();
-		$('#on').show();
+	$('#meta-logo').click(function() {
+		if ( t.on == false ){
+			$.get ( urlprefix+'cgi-bin/on' );
+			
+			t.on = true;
+			$('#content').animate({'height': 'toggle', 'opacity': 1 }, 500, function(){
+				$('#meta-logo').removeClass('off').addClass('on');
+				$('#on-off-info').html('');
+			});
+		} else {
+			$.get ( urlprefix+'cgi-bin/off' );
+			t.on = false;
+			$('#content').animate({ 'height': 'toggle', 'opacity': 0 }, 500, function(){
+				$(this).hide();
+				$('#meta-logo').removeClass('on').addClass('off');
+			});
+			$('#on-off-info').html('Lounge Control is deactivated. Press the logo to activate.');
+		}
 	});
-	
-	//turn the amplifier on
-	$('#on').live('click', function() {
-		$.get ( urlprefix+'cgi-bin/on' );
-		$(this).hide();
-		$('#off').show();
-	});
-	
-	
 	
 	/***************************** START OF VOLUME ************************************/
 	
@@ -306,7 +314,9 @@ function renderXML (xml, t ) {
 		var gui = $('#video-player-gui');
 		gui.animate({
 				height: 'toggle'
-			}, 100 );
+			}, 100, function(){
+				gui.removeClass('hidden');1
+			});
 			
 		if ( t.video_player_shown == false ) {
 			
@@ -343,7 +353,7 @@ function renderXML (xml, t ) {
 	
 	$('#vid-load-button').live ('click', function(){
 		var search_term = $('#vid-load-text').html();
-		$.get( urlprefix+'cgi-bin/search?'+search_term );
+		$.get( urlprefix+'cgi-bin/playlink?'+search_term );
 	});
 	
 	
@@ -357,7 +367,7 @@ function renderXML (xml, t ) {
 				$('#search-and-play-container').hide();
 				t.search_and_play_visible = false;
 			} 
-			$('#life-stream-container').show();
+			$('#life-stream-container').show().removeClass('hidden');
 			t.upload_stream_visible = true;
 		}
 	});
@@ -371,7 +381,7 @@ function renderXML (xml, t ) {
 				$('#life-stream-container').hide();
 				t.upload_stream_visible = false;
 			} 
-			$('#search-and-play-container').show();
+			$('#search-and-play-container').show().removeClass('hidden');
 			t.search_and_play_visible = true;
 		}
 	});
