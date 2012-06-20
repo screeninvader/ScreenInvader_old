46281dd6f4e5c3232c64b544387c3f83dc32f88f
kallaballa <amir@viel-zu.org>
2012-06-20 15:51:12 +0200

    use ext4 using writeback modeand without journaling

--- makestick.sh
+++ makestick.sh
@@ -5,33 +5,32 @@ DEVICE="$1"
 SIZE="$2"
 
 function printUsage() {
-	cat 1>&2 <<EOUSAGE
+  cat 1>&2 <<EOUSAGE
 makestick - Prepare a file system for installation of the Lounge Media Center
 
 Usage: $0 <device-file> [<sizeInM>]
 
 <device-file>    a block special device.
 <sizeInM>        the size of the resulting file system in megabytes.
-EOUSAGE 
-
+EOUSAGE
   exit 1
 }
 
 function makeSyslinuxConf() {
-  uuid="`blkid $DEVICE*1 | cut -d "\"" -f2`"
-  templates/syslinux_cfg $uuid > "$1/boot/syslinux/syslinux.cfg" 
+  uuid="`blkid $DEVICE*1 | cut -d '"' -f2`"
+  templates/syslinux_cfg "$uuid" > "$1/boot/syslinux/syslinux.cfg" 
 }
+
+export BOOTSTRAP_LOG="makestick.log"
+source "$MAKEPARTITION_DIR/.functions.sh"
 
 [ $# -eq 0 -o $# -gt 2 ] && printUsage;
 [ ! -b "$DEVICE" ] &&  error "Not a block device: $DEVICE";
 [ -z "$SIZE" ] && SIZE=400
 [ printf "%d" $SIZE &> /dev/null -o $SIZE -lt 100 ] && error "Invalid size: $SIZE"
 
-export BOOTSTRAP_LOG="makestick.log"
-source "$MAKEPARTITION_DIR/.functions.sh"
-
-check "Write zeros to device" \
-  "dd if=/dev/zero of=$DEVICE bs=1M count=$[$SIZE + 1]" 
+#check "Write zeros to device" \
+#  "dd if=/dev/zero of=$DEVICE bs=1M count=$[$SIZE + 1]" 
 
 check "Make disk label" \
   "parted -s $DEVICE mklabel msdos"
@@ -41,6 +40,12 @@ check "Make partition" \
 
 check "Make filesystem" \
   "mkfs.ext4 $DEVICE*1"
+
+check "Enable writeback mode" \
+  "tune2fs -o journal_data_writeback $DEVICE*1"
+
+check "Disable journaling" \
+  "tune2fs -O ^has_journal $DEVICE*1"
 
 tmpdir=`mktemp  -d`
 check "Make temporary mount dir" \
@@ -52,7 +57,7 @@ check "Mount file system" \
 check "Prune syslinux dir" \
   "mkdir -p $tmpdir/boot/syslinux/"
 
-check "Instal extlinux" \
+check "Install extlinux" \
   "extlinux --install  $tmpdir/boot/syslinux"
 
 makeSyslinuxConf "$tmpdir"
@@ -62,7 +67,6 @@ check "Make syslinux.cfg" \
 check "Umount file system" \
 	"umount $DEVICE*1"
 
-rmdir $tmpdir
 check "Remove temporary mount dir" \
   "rmdir $tmpdir"
 
