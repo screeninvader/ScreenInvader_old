d975ff7f9a2c44a17b7bc47ea04c62d909696ab7
kallaballa <amir@viel-zu.org>
2012-06-20 13:00:33 +0200

    moved /home/elchaschab to /lounge

--- data/lounge/.bashrc
+++ data/lounge/.bashrc
@@ -1,7 +1,7 @@
 # ~/.bashrc: executed by bash(1) for non-login shells.
 # see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 # for examples
-HOME="/home/lounge"
+HOME="/lounge"
 PATH="$HOME/bin:$PATH"
 unset HISTFILE
 HISTSIZE=0
--- data/lounge/xsession.sh
+++ data/lounge/xsession.sh
@@ -2,6 +2,6 @@ export DISPLAY=:0
 setterm -blank 0 -powersave off -powerdown 0 &
 xhost + &
 xset dpms force on
-nice -n19 midori -c /home/lounge/midori_mpdstat/ --class="MPDstat" --name="MPDstat" http://localhost/mpdstat.html &
+nice -n19 midori -c /lounge/.config/midori_mpdstat/ --class="MPDstat" --name="MPDstat" http://localhost/mpdstat.html &
 sudo -u lounge awesome
 
