baeb80d3e0c76abfc720e1a761e1067d7001f9d9
kallaballa <amir@viel-zu.org>
2012-06-28 19:24:38 +0200

    orf live stream support

---
+++ data/lounge/bin/orfget.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# LoungeMC - A content centered media center 
+#  Copyright (C) 2012 Amir Hassan <amir@viel-zu.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+set -x
+
+function wgeto {
+  wget -O - -q "$1"
+}
+
+if [ $# -ne 1 ]; then
+   echo "Usage: orfget.sh <videopageurl>" 1>&2
+   exit 1
+fi
+
+ASX_URL="`wgeto "$1" |  grep "src=.*mms" | cut -d'"' -f2`"
+echo $ASX_URL
+
