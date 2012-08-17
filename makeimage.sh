3077d0238690e654855e6590ef4977b968b33021
kallaballa <amir@viel-zu.org>
2012-08-17 03:18:37 +0200

    add commandline switches and usage. check everything

--- makeimage.sh
+++ makeimage.sh
@@ -17,36 +17,92 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-
 dir="`dirname $0`"
 MAKEIMAGE_DIR="`cd $dir; pwd`"
 
+export BOOTSTRAP_LOG="makeimage.log"
+
 source "$MAKEIMAGE_DIR/.functions.sh"
 
-IMAGE_FILE="tsarbomba.dd"
-IMAGE_SIZE=$1
+function absDir() {
+  dir="`dirname $1`"
+  absdir="`cd $dir; pwd`"
+  echo $absdir
+}
+
+function absPath() {
+  dir="`dirname $1`"
+  base="`basename $1`"
+  absdir="`cd $dir; pwd`"
+  echo $absdir/$base
+}
+
+function printUsage() {
+  cat 1>&2 <<EOUSAGE
+makeimage.sh - Prepare a disk image for installation of the ScreenInvader system.
+
+Usage: $0 [-z][-c <chrootdir>][-s <sizeInM>] <target>
+<target>	   the target location for the disk image
+
+Options:
+  -z               write zeroes to the device before creating the partition
+  -s <sizeInM>	   overwrite the default (= 500MB) disk image size.
+  -c <chrootdir>   after creating the disk image mount it in the specified directory
+EOUSAGE
+  exit 1
+}
+
+WRITE_ZEROES=
+IMAGE_SIZE=500
+IMAGE_FILE=
+CHROOT_DIR=
+
+while getopts 'zs:c:' c
+do
+  case $c in
+    z) WRITE_ZEROES="YES";;
+    s) IMAGE_SIZE="$OPTARG";;
+    c) CHROOT_DIR="$OPTARG";;
+    \?) printUsage;;
+  esac
+done
+
+shift $(($OPTIND - 1))
+
+[ $# -ne 1 ] && printUsage
+
+IMAGE_FILE="`absPath $1`"
 LOOPBACK_DEVICE=`losetup -f`
-CHROOT_DIR="../chrootdir"
+MAKESTICK_OPTS="-s $IMAGE_SIZE"
+[ -n "$WRITE_ZEROES" ] && MAKESTICK_OPTS="$MAKESTICK_OPTS -z"
 
-[ -z $IMAGE_SIZE ] && IMAGE_SIZE="500"
-
-check "Mountpoint $CHROOT_DIR is unused" \
+[ -n "$CHROOT_DIR" ] && check "Mountpoint $CHROOT_DIR is unused" \
   "! mountpoint -q $CHROOT_DIR"
 
 check "Creating disk image file $IMAGE_FILE of size $IMAGE_SIZE MB" \
-  "dd if=/dev/zero of=$IMAGE_FILE bs=1M count=1 seek=$IMAGE_SIZE > /dev/null"
+  "dd if=/dev/zero of=$IMAGE_FILE bs=1M count=1 seek=$[ $IMAGE_SIZE - 1 ] > /dev/null"
 
 check "Setting up disk image file on loopback device $LOOPBACK_DEVICE" \
   "losetup $LOOPBACK_DEVICE $IMAGE_FILE"
 
-./makestick.sh "$LOOPBACK_DEVICE" "$@"
+
+./makestick.sh $MAKESTICK_OPTS "$LOOPBACK_DEVICE"
+check "Run makestick.sh $MAKESTICK_OPTS $LOOPBACK_DEVICE" \
+          "[ $? -eq 0 ]"
 
 OFFSET=`parted -s -m $LOOPBACK_DEVICE unit B print | grep "^1:" | cut -f 2 -d ":" | tr B ' '`
 
-sync
+check "Read partition offset" \
+  "[ $? -eq 0 ]"
+  
+check "Sync" \
+  "sync"
 
 check "Detaching disk image file" \
   "losetup -d $LOOPBACK_DEVICE"
 
-check "Mounting screen invader partition on $CHROOT_DIR" \
+[ -n "$CHROOT_DIR" ] && check "Mounting screen invader partition on $CHROOT_DIR" \
   "mount $IMAGE_FILE $CHROOT_DIR -o loop,offset=$OFFSET"
+
+exit 0
+
