461f172b5af6176cb6a3bd9fdb6fe0efc9cb91b4
kallaballa <amir@viel-zu.org>
2012-12-03 18:09:38 +0100

    screen resolution adaption

--- src/lounge/xsession.sh
+++ src/lounge/xsession.sh
@@ -1,32 +1,16 @@
-#!/bin/bash
-#
-# ScreenInvader - A shared media experience. Instant and seamless.
-#  Copyright (C) 2012 Amir Hassan <amir@viel-zu.org>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-
 export DISPLAY=:0
 
-sudo -u lounge bash -c "sleep 1; xdotool mousemove 1900 1200"
+killall janosh -d &
+sudo /lounge/bin/gainroot /lounge/bin/janosh -d &
+sudo -u lounge /lounge/bin/janosh -d &
+
 /root/triggers/display apply
+
 sudo -u lounge xhost +
 sudo -u lounge /lounge/bin/showip &>/dev/null &
 sudo -u lounge /lounge/bin/player mplayer_start &>/dev/null &
 sudo -u lounge /lounge/bin/player refresh
 
+# give midori a fixed geometry because sometimes it fails to adapt to resolution changes
 exec evilwm -bw 0 -fn "-misc-topaz a500a1000a2000-medium-r-normal--0-240-0-0-c-0-iso8859-1"  -app midori -g "$(/lounge/bin/janosh -r get /display/resolution/value)"+0+0
-#exec evilwm -fn "-misc-topaz a500a1000a2000-medium-r-normal--0-240-0-0-c-0-iso8859-1"
 
