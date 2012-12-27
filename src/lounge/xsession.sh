ca3b7afb98977f14c3c02ae5cede0960e418039f
kallaballa <amir@viel-zu.org>
2012-12-27 22:02:11 +0100

    make sure evilwm isn't running anymore

--- src/lounge/xsession.sh
+++ src/lounge/xsession.sh
@@ -10,5 +10,6 @@ sudo -u lounge /lounge/bin/player mplayer_start &>/dev/null &
 sudo -u lounge /lounge/bin/player refresh
 
 # give midori a fixed geometry because sometimes it fails to adapt to resolution changes
+killall evilwm
 exec evilwm -bw 0 -fn "-misc-topaz a500a1000a2000-medium-r-normal--0-240-0-0-c-0-iso8859-1"  -app midori -g "$(/lounge/bin/janosh -r get /display/resolution/value)"+0+0
 
