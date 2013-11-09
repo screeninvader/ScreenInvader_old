3737f70450ae66d36280857242a968d68c3015d0
kallaballa <amir@viel-zu.org>
2013-11-09 18:57:31 +0100

    fixed browser geometry

--- src/lounge/xsession.sh
+++ src/lounge/xsession.sh
@@ -11,5 +11,5 @@ sudo -u lounge /lounge/bin/player refresh
 
 # give midori a fixed geometry because sometimes it fails to adapt to resolution changes
 killall evilwm
-exec evilwm -bw 0 -fn "-misc-topaz a500a1000a2000-medium-r-normal--0-240-0-0-c-0-iso8859-1"  -app midori -g "$(/lounge/bin/janosh -r get /display/resolution/value)"+0+0
+exec evilwm -bw 0 -fn "-misc-topaz a500a1000a2000-medium-r-normal--0-240-0-0-c-0-iso8859-1"  -app Midori -g "$(/lounge/bin/janosh -r get /display/resolution/value)"+0+0
 
