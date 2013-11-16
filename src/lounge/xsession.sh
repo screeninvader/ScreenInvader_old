7582b913b2f94acf49b6a5c681354286f8fc6658
kallaballa <amir@viel-zu.org>
2013-11-16 17:50:53 +0100

    set default resolution to 1024x768

--- src/lounge/xsession.sh
+++ src/lounge/xsession.sh
@@ -11,5 +11,9 @@ sudo -u lounge /lounge/bin/player refresh
 
 # give midori a fixed geometry because sometimes it fails to adapt to resolution changes
 killall evilwm
+xrandr --output VGA1 --mode 1024x768
+xrandr --output HDMI1 --mode 1024x768
+xrandr --output LVDS1 --off
 exec evilwm -bw 0 -fn "-misc-topaz a500a1000a2000-medium-r-normal--0-240-0-0-c-0-iso8859-1"  -app Midori -g "$(/lounge/bin/janosh -r get /display/resolution/value)"+0+0
+
 
