f3b7f9315e073a87795b59d40718abde8b410a84
kallaballa <amir@viel-zu.org>
2013-12-15 20:57:42 +0100

    add setResolution trigger

--- src/lounge/xsession.sh
+++ src/lounge/xsession.sh
@@ -3,6 +3,7 @@
 set -x
 export DISPLAY=:0
 export HOME=/root
+export PATH="/lounge/bin/:$PATH"
 mkdir -p /var/run/player/
 chown lounge:lounge /var/run/player/
 sudo -u lounge /lounge/triggers/sound reload
@@ -15,10 +16,14 @@ sudo -u lounge /lounge/bin/player refresh
 
 # give midori a fixed geometry because sometimes it fails to adapt to resolution changes
 killall evilwm
-resolution="$(/lounge/bin/janosh -r get /display/resolution/value)"
-xrandr --output VGA1 --mode $resolution
-xrandr --output HDMI1 --mode $resolution
-#xrandr --output LVDS1 --off 
+
+resolution="$(sudo -u lounge /lounge/bin/janosh -r get /display/resolution)"
+sudo -u lounge /lounge/bin/janosh -t set /display/resolution $resolution
+
+# apply real resolution
+resolution="`xrandr -q | fgrep "*" | tr -s " " | cut -d" " -f2`"
+sudo -u lounge /lounge/bin/janosh -t set /display/resolution $resolution
+
 xbindkeys & 
 exec evilwm -bw 0 -fn "-misc-topaz a500a1000a2000-medium-r-normal--0-240-0-0-c-0-iso8859-1"  -app Midori -g "$resolution"+0+0
 ) &> /tmp/xsession
