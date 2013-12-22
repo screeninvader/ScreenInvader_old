70ae38f07d82332aeae31dbb05f41f38871a7591
kallaballa <amir@viel-zu.org>
2013-12-23 00:58:31 +0100

    install packages during bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -182,32 +182,32 @@ function doPackageConf() {
 }
 
 function doCopy() {
-  check "Copy system changes" \
-    "cd $BOOTSTRAP_DIR/src; rsync -axh etc usr $CHROOT_DIR/"
+  check "Make install directory" \
+    "mkdir -p $CHROOT_DIR/install/"
+
+  check "Copy debian packages" \
+    "cp $BOOTSTRAP_DIR/packaging/*.deb $CHROOT_DIR/install/"
   
-  check "Sync lounge changes" \
-    "cd $BOOTSTRAP_DIR/src; rsync -axh --delete lounge $CHROOT_DIR/"
+  check "install core packages" \
+    "$CHRT dpkg -i /install/screeninvader-core-all.deb"
 
-  check "Sync root changes" \
-    "cd $BOOTSTRAP_DIR/src; rsync -axh --delete root $CHROOT_DIR/"
+  check "install config packages" \
+    "$CHRT dpkg -i --force-all /install/screeninvader-config-all.deb"
 
-  check "Sync setup changes" \
-    "cd $BOOTSTRAP_DIR/src; rsync -axh --delete setup $CHROOT_DIR/"
+  check "install misc packages" \
+    "$CHRT dpkg -i /install/screeninvader-misc-all.deb"
+
+  check "install binary package" \
+    "$CHRT dpkg -i /install/screeninvader-binaries-all.deb"
+
+  check "Remove install directory" \
+    "rm -r $CHROOT_DIR/install/"
 
 #  check "Copy plymouth theme" \
 #    "cp -a $BOOTSTRAP_DIR/themes/screeninvader $CHROOT_DIR/usr/share/plymouth/themes/"
 
-  check "Copy xosd lib" \
-    "cp -a $BOOTSTRAP_DIR/build/libxosd.so.2.2.14 $CHROOT_DIR/usr/lib/"
-
   check "ldconfig" \
     "$CHRT ldconfig"
-
-  check "Copy osd binary"  \
-    "cp -a $BOOTSTRAP_DIR/build/osd $CHROOT_DIR/lounge/bin/"
-
-  check "Copy janosh binary"  \
-    "cp -a $BOOTSTRAP_DIR/build/janosh $CHROOT_DIR/lounge/bin/"
 
 #  check "Update plymouth theme" \
 #    "$CHRT plymouth-set-default-theme -R screeninvader"
