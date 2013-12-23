1b8462c5ae58dcde164caf1c1d7b7e8ab51a16fa
kallaballa <amir@viel-zu.org>
2013-12-23 02:24:35 +0100

    fix sudoers file permissions

--- packaging/makeconfig.sh
+++ packaging/makeconfig.sh
@@ -7,5 +7,8 @@ fi
 ./makecontrol.sh config $1 > screeninvader-config/DEBIAN/control
 rm -r screeninvader-config/etc
 cp -a ../src/etc screeninvader-config/
+chown root:root screeninvader-config/etc/sudoers 
+chown root:root screeninvader-config/etc/passwd
+chmod 0440 screeninvader-config/etc/sudoers
 dpkg-deb -b screeninvader-config/ screeninvader-config-all.deb
 dpkg-sig --sign builder screeninvader-config-all.deb
