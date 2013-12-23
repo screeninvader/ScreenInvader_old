6d04708c99320668e2d7c16233212c3c521088c8
kallaballa <amir@viel-zu.org>
2013-12-23 01:47:54 +0100

    make control files automatically

---
+++ packaging/makecontrol.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: makecontrol.sh <component> <version>"
+fi
+
+
+cat <<EOCONTROL
+Package: screeninvader-$1
+Version: $2
+Architecture: i386
+Maintainer: Amir Hassan <amir@viel-zu.org>
+Installed-Size: 1099
+Section: misc
+Priority: optional
+Homepage: http://screeninvader.com
+Description: ScreenInvader $1 files
+EOCONTROL
+
