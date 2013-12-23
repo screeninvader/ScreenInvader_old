01444f04c8b8d0b17f3fa8eae7474a823cb9a832
kallaballa <amir@viel-zu.org>
2013-12-23 04:07:07 +0100

    fix file permissions

--- packaging/makecore.sh
+++ packaging/makecore.sh
@@ -7,5 +7,7 @@ fi
 ./makecontrol.sh core $1 > screeninvader-core/DEBIAN/control
 rm -r screeninvader-core/root screeninvader-core/lounge screeninvader-core/setup
 cp -a ../src/setup ../src/lounge ../src/root screeninvader-core/
+chown -r root:root screeninvader-core/root
+chown -r 1000:1000 screeninvader-core/lounge
 dpkg-deb -b screeninvader-core/ screeninvader-core-all.deb
 dpkg-sig --sign builder screeninvader-core-all.deb
